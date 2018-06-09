fc4692626f7908f96503ae654f6dbfa7f61ae3ed
Eric Huiza <erichuiza@Erics-MacBook-Pro-2.local>
2018-06-09 15:11:02 -0500

    initial commit

---
+++ src/main/java/com/prosports/prof1/Prof1Application.java
@@ -0,0 +1,12 @@
+package com.prosports.prof1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prof1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Prof1Application.class, args);
+	}
+}
