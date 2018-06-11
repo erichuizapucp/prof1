78d8e4a4c5a56c491da5cedcfae585aa8b02969a
erichuiza <eric.huiza@pucp.edu.pe>
2018-06-10 23:40:21 -0500

    remover lombock

--- src/main/java/com/prosports/prof1/Entidades/Merchandising.java
+++ src/main/java/com/prosports/prof1/Entidades/Merchandising.java
@@ -1,13 +1,8 @@
 package com.prosports.prof1.Entidades;
 
-import lombok.Getter;
-import lombok.Setter;
-
 import javax.persistence.*;
 
 @Entity
-@Getter
-@Setter
 public class Merchandising {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,6 +31,46 @@ public class Merchandising {
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
+        this.imagen = imagen;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
         this.imagen = imagen;
     }
 }
